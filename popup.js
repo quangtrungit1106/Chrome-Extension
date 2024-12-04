aff74e840dead93c16b4d2148c937e02bd963a54
QuangTrung <117032549+quangtrungit1106@users.noreply.github.com>
2024-12-04 20:41:16 +0700

    init

---
+++ popup.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Function to call the API
+  function checkPhishingStatus() {
+      fetch('https://127.0.0.1:5000/predict', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ html: document.documentElement.outerHTML })
+      })
+        .then(response => response.json())
+        .then(data => {
+              if (data.result === 1) {
+                  document.getElementById('safe').style.display = 'block';
+                  document.getElementById('loading').style.display = 'none';
+                  document.getElementById('cheat').style.display = 'none';
+              } else if (data.result === 0) {
+                  document.getElementById('safe').style.display = 'none';
+                  document.getElementById('cheat').style.display = 'block';
+                  document.getElementById('loading').style.display = 'none';
+              }
+          })
+          .catch(error => {
+              console.error('Error fetching the API:', error);
+              document.getElementById('loading').textContent = 'Error loading data';
+          });
+  }
+
+  checkPhishingStatus();
+});
