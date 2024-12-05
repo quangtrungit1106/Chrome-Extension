c332a3c5ec447bd1a5acd15fc267601ddc48dbd3
quangtrungit1106 <quangtrungit1106@gmail.com>
2024-12-05 07:44:59 +0700

    fix error

--- content.js
+++ content.js
@@ -1,49 +1,46 @@
 window.addEventListener('load', function () {
-    // Tạo một div thông báo
-    const notification = document.createElement('div');
-    notification.innerHTML = `
-       <div style="position: fixed; top: 0; left: 50%; transform: translateX(-50%); background: white; border: 1px solid #ccc; border-radius: 20px; box-shadow: 0 4px 8px rgba(0,0,0,0.1); z-index: 10000; text-align: center; display: flex; align-items: center; padding: 10px;">
-        <p style="margin: 0; flex-grow: 1;">Loading...</p>
-        <button style="display: none; margin-left: 10px; background: transparent; border: none; font-size: 16px; cursor: pointer;">✖</button>
-      </div>
-    `;
-    document.body.appendChild(notification);
+  // Tạo một div thông báo
+  const notification = document.createElement('div');
+  notification.innerHTML = `
+     <div style="position: fixed; top: 0; left: 50%; transform: translateX(-50%); background: white; border: 1px solid #ccc; border-radius: 20px; box-shadow: 0 4px 8px rgba(0,0,0,0.1); z-index: 10000; text-align: center; display: flex; align-items: center; padding: 10px;">
+      <p style="margin: 0; flex-grow: 1;">Loading...</p>
+      <button style="display: none; margin-left: 10px; background: transparent; border: none; font-size: 16px; cursor: pointer;">✖</button>
+    </div>
+  `;
+  document.body.appendChild(notification);
 
-    fetch("https://127.0.0.1:5000/predict", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ html: document.documentElement.outerHTML })
+  fetch("https://127.0.0.1:5000/predict", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ html: document.documentElement.outerHTML })
+  })
+    .then(response => response.json())
+    .then(data => {
+      const message = notification.querySelector('p');
+      const closeButton = notification.querySelector('button');
+      const notificationBox = notification.firstElementChild;
+      if(data.result === 1){
+        message.textContent = 'Website không có dấu hiệu lừa đảo!';
+        notificationBox.style.backgroundColor = '#d4edda'; // Màu xanh lá cây nhạt
+        notificationBox.style.borderColor = '#c3e6cb'; // Màu viền xanh lá cây nhạt
+         setTimeout(() => {
+          notification.style.display = 'none';
+        }, 3000);
+      } else {
+        message.textContent = 'Website không an toàn, có dấu hiệu lừa đảo!';
+        notificationBox.style.backgroundColor = '#f8d7da'; // Màu đỏ nhạt
+        notificationBox.style.borderColor = '#f5c6cb'; // Màu viền đỏ nhạt
+        closeButton.style.display = 'block';
+        closeButton.addEventListener('click', () => {
+          notification.style.display = 'none';
+        });
+      }
     })
-      .then(response => response.json())
-      .then(data => {
-        const message = notification.querySelector('p');
-        const closeButton = notification.querySelector('button');
-        const notificationBox = notification.firstElementChild;
-        if(data.result === 1){
-          message.textContent = 'Website không có dấu hiệu lừa đảo!';
-          notificationBox.style.backgroundColor = '#d4edda'; // Màu xanh lá cây nhạt
-          notificationBox.style.borderColor = '#c3e6cb'; // Màu viền xanh lá cây nhạt
-           setTimeout(() => {
-            notification.style.display = 'none';
-          }, 3000);
-        } else {
-          message.textContent = 'Website không an toàn, có dấu hiệu lừa đảo!';
-          notificationBox.style.backgroundColor = '#f8d7da'; // Màu đỏ nhạt
-          notificationBox.style.borderColor = '#f5c6cb'; // Màu viền đỏ nhạt
-          closeButton.style.display = 'block';
-          closeButton.addEventListener('click', () => {
-            notification.style.display = 'none';
-          });
-        }
-      })
-      .catch(error => {
-        notification.querySelector('p').textContent = 'Error analyzing page.';
-        console.error(error);
-      });
+    .catch(error => {
+      notification.querySelector('p').textContent = 'Error loading data.';
+      console.error(error);
+      setTimeout(() => {
+        notification.style.display = 'none';
+      }, 3000);
+    });
 });
-
-
-
-
-
-  
